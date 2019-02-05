b555947ff255dedf2dba2e5037d247fb93f07d25
sanlugaTec <luis.garcia@tec.mx>
2019-02-05 10:24:35 -0600

    inicio

---
+++ comunica_BT.ino
@@ -0,0 +1,22 @@
+#include <SoftwareSerial.h>   // Incluimos la librería  SoftwareSerial  
+SoftwareSerial BT(8,9);    // Definimos los pines TX y RX del Arduino conectados al Bluetooth
+ 
+void setup()
+{
+  BT.begin(9600);       // Inicializamos el puerto serie BT que hemos creado
+  Serial.begin(9600);   // Inicializamos  el puerto serie  
+}
+ 
+void loop()
+{
+  //Serial.println(BT.available());
+  if(BT.available())    // Si llega un dato por el puerto BT se envía al monitor serial
+  {
+    Serial.write(BT.read());
+  }
+ 
+  if(Serial.available())  // Si llega un dato por el monitor serial se envía al puerto BT
+  {
+     BT.write(Serial.read());
+  }
+}
